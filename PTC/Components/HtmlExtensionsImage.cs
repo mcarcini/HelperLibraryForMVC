1fba22a053da6768ac984028b1bfc12556f49293
Marco Carcini <marco.carcini@hotmail.com>
2020-04-10 12:20:24 -0500

    Create a MVC Helper with the use of StringBuilder class

---
+++ PTC/Components/HtmlExtensionsImage.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Text;
+using System.Web.Mvc;
+
+
+namespace PTC
+{
+    public static class HtmlExtensionsImage
+    {
+        public static MvcHtmlString Image(this HtmlHelper htmlHelper,
+            string src,
+            string altText)
+        {
+            StringBuilder sb = new StringBuilder(512);
+            sb.AppendFormat("<img src='{0}' alt='{1}' />",src,altText);
+
+            return MvcHtmlString.Create(sb.ToString());
+        }
+    }
+}
