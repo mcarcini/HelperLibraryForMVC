5e9408701c0faffac8c1b78720efcc0494c932a3
Marco Carcini <marco.carcini@hotmail.com>
2020-04-11 23:36:41 -0500

    .

---
+++ PTC/Components/HtmlExtensionsButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTC
+{
+    public static class HtmlExtensionsButton
+    {
+        public static MvcHtmlString BoostrapButton(this HtmlHelper htmlHelper,
+            string innerHtml, 
+            string cssClass,
+            string name,
+            string id)
+        {
+            TagBuilder tb = new TagBuilder("button");
+
+            if (string.IsNullOrWhiteSpace(cssClass))
+            {
+                cssClass = "btn-primary";
+            }            
+
+            tb.AddCssClass(cssClass);
+            tb.AddCssClass("btn");
+
+            HtmlExtensionsCommon.AddName(tb, name, id);
+
+            tb.InnerHtml = innerHtml;
+
+            tb.MergeAttribute("type", "submit");
+
+            return MvcHtmlString.Create(tb.ToString());
+        }
+    }
+}
---
+++ PTC/Components/HtmlExtensionsCommon.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace PTC
+{
+    public static class HtmlExtensionsCommon
+    {
+        public static void AddName(TagBuilder tb, string name, string id) {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                name = TagBuilder.CreateSanitizedId(name);
+                tb.MergeAttribute("name", name);
+            }
+
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                id = TagBuilder.CreateSanitizedId(id);
+                tb.GenerateId(id);
+            }
+
+                
+        }
+    }
+}
